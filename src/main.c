79a907929a06984803b056d3daf34b75a675420c
Gabriel Kis <gabrielkis65@gmail.com>
2025-07-14 14:38:56 -0300

    Need to test - Added I2S driver for LED Strip

--- src/main.c
+++ src/main.c
@@ -6,146 +6,84 @@
 #include <zephyr/kernel.h>
 #include <app_version.h>
 
+/*
+ * Copyright (c) 2017 Linaro Limited
+ * Copyright (c) 2018 Intel Corporation
+ * Copyright (c) 2024 TOKITA Hiroshi
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <string.h>
+
+#define LOG_LEVEL 4
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main);
 
 // #include <zephyr/kernel.h>
-// #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/led_strip.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/sys/util.h>
 
-// #include "message_main_bubble.h"
+#define STRIP_NODE		DT_ALIAS(led_strip)
 
-// /* PWM device for servo control */
-// #define BUBBLE_THREAD_STACK_SIZE 1024
-// #define BUBBLE_THREAD_PRIORITY 7
+#if DT_NODE_HAS_PROP(DT_ALIAS(led_strip), chain_length)
+#define STRIP_NUM_PIXELS	DT_PROP(DT_ALIAS(led_strip), chain_length)
+#else
+#error Unable to determine length of LED strip
+#endif
 
-// #if !DT_NODE_HAS_STATUS(DT_ALIAS(led0), okay)
-// #error "LED0 GPIO node is not ready"
-// #endif
+#define DELAY_TIME K_MSEC(CONFIG_SAMPLE_LED_UPDATE_DELAY)
 
-// static const struct gpio_dt_spec bubble_io = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+#define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
 
-// // Thread initialization
-// K_THREAD_STACK_DEFINE(bubble_thread_stack, BUBBLE_THREAD_STACK_SIZE);
-// struct k_thread bubble_thread_data;
-// k_tid_t bubble_thread_id;
+static const struct led_rgb colors[] = {
+	RGB(CONFIG_SAMPLE_LED_BRIGHTNESS, 0x00, 0x00), /* red */
+	RGB(0x00, CONFIG_SAMPLE_LED_BRIGHTNESS, 0x00), /* green */
+	RGB(0x00, 0x00, CONFIG_SAMPLE_LED_BRIGHTNESS), /* blue */
+};
 
-// // -----------------------------------
-// // Static functions
-// // -----------------------------------
-// static void init_io(void)
-// {
-//     if (!device_is_ready(bubble_io.port)) {
-//         printk("Error: IO device %s is not ready\n", bubble_io.port->name);
-//         return;
-//     }
+static struct led_rgb pixels[STRIP_NUM_PIXELS];
 
-//     int ret = gpio_pin_configure_dt(&bubble_io, GPIO_OUTPUT_ACTIVE);
-//     if (ret < 0) {
-//         printk("Error %d: failed to configure LED pin\n", ret);
-//     }
-
-//     gpio_pin_set_dt(&bubble_io, 0);
-// }
-
-// void handle_main_msg(void)
-// {
-//     static struct main_to_bubble_msg_t msg = {};
-//     if (recv_message_main_to_bubble(&msg) != 0) {
-//         return;
-//     }
-
-//     switch (msg.type) {
-//         case MAIN_CMD_BUBBLE_ON:
-//             printk("BUBBLE: On\n");
-//             gpio_pin_set_dt(&bubble_io, 1);
-//             break;
-//         case MAIN_CMD_BUBBLE_OFF:
-//             printk("BUBBLE: Off\n");
-//             gpio_pin_set_dt(&bubble_io, 0);
-//             break;
-//         default:
-//             printk("Unknown HMI Command: %d\n", msg.type);
-//             break;
-//     }
-// }
-
-// #include "uart_cmd.h"
-// #include "bubble_control.h"
-// #include "iot_thread.h"
-
-// #include "message_hmi_main.h"
-// #include "message_main_bubble.h"
-// #include "message_iot_main.h"
-
-// void handle_hmi_msg(void)
-// {
-//     static struct hmi_msg_t hmi_msg = {};
-//     if (recv_message_hmi_to_main(&hmi_msg) != 0) {
-//         return;
-//     }
-
-//     switch (hmi_msg.module) {
-//         case MOD_MAIN:
-//             //printk("HMI Command to Main Thread\n");
-//             break;
-//         case MOD_BUBBLE:
-//             //printk("HMI Command to Bubble Thread\n");
-//             struct main_to_bubble_msg_t main_to_bubble = {0};
-//             if (hmi_msg.type == HMI_CMD_BUBBLE_ON) {
-//                 main_to_bubble.type = MAIN_CMD_BUBBLE_ON;
-//             } else if (hmi_msg.type == HMI_CMD_BUBBLE_OFF) {
-//                 main_to_bubble.type = MAIN_CMD_BUBBLE_OFF;
-//             } else {
-//                 return;
-//             }
-//             send_message_main_to_bubble(&main_to_bubble);
-//             //TODO: Handle return from message sent
-//             break;
-//         default:
-//             printk("Command not recognized from HMI: %02X", hmi_msg.type);
-//             break;
-//     }
-// }
-
-// void handle_iot_msg(void)
-// {
-//     static struct iot_to_main_msg_t iot_msg = {};
-//     if (recv_message_iot_to_main(&iot_msg) != 0) {
-//         return;
-//     }
-
-//     struct main_to_bubble_msg_t main_to_bubble = {0};
-//     switch (iot_msg.type) {
-//         case IOT_CMD_BUBBLE_ON:
-//             main_to_bubble.type = MAIN_CMD_BUBBLE_ON;
-//             //printk("HMI Command to Main Thread\n");
-//             break;
-//         case IOT_CMD_BUBBLE_OFF:
-//             main_to_bubble.type = MAIN_CMD_BUBBLE_OFF;
-//             break;
-//         default:
-//             printk("Command not recognized from IOT: %02X", iot_msg.type);
-//             return;
-//     }
-//     send_message_main_to_bubble(&main_to_bubble);
-// }
+static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 
 int main(void)
 {
     printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
 
-    // bubble_thread_start();
-    // //uart_thread_start();
-    // iot_thread_start();
+	size_t color = 0;
+	int rc;
+
+	if (device_is_ready(strip)) {
+		LOG_INF("Found LED strip device %s", strip->name);
+	} else {
+		LOG_ERR("LED strip device %s is not ready", strip->name);
+		return 0;
+	}
+
+	LOG_INF("Displaying pattern on strip");
 
     while (1) {
-        // Handle communication with all threads
-        //handle_hmi_msg();
-        // handle_iot_msg();
-        // Read data from HMI Thread
-        // Read data from Wifi/MQTT
-        // Write data to Servo Thread
-        // Read data from servo thread
         k_msleep(500);  // Delay to avoid CPU usage
         printk("AIAIAIAI Bixin\n");
+
+		for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
+			memset(&pixels, 0x00, sizeof(pixels));
+			memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
+
+			rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
+			if (rc) {
+				LOG_ERR("couldn't update strip: %d", rc);
+			}
+
+			k_sleep(DELAY_TIME);
+		}
+
+		color = (color + 1) % ARRAY_SIZE(colors);
+
+
     }
 
     return 0;
