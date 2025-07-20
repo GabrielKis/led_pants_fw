468163d08ef2f913513c012f218ac96702185b8a
Gabriel Kis <gabrielkis65@gmail.com>
2025-07-20 20:10:31 -0300

    ADC with histeresys sending signal to LEDRGB

--- src/main.c
+++ src/main.c
@@ -90,6 +90,89 @@ void send_msg_main_led_rgb(void)
     }
 }
 
+// ------------- ADC -------------------
+// ADC Global variables
+static uint32_t count = 0;
+static uint16_t buf;
+static struct adc_sequence sequence = {
+    .buffer = &buf,
+    /* buffer size in bytes, not number of samples */
+    .buffer_size = sizeof(buf),
+};
+static const struct adc_dt_spec *adc_channel = &adc_channels[0];
+
+#define STOMP_SIGNAL_ON_THRESHOLD 1500U // mV threshold for stomp signal ON
+#define STOMP_SIGNAL_OFF_THRESHOLD 500U // mV threshold for stomp signal OFF
+
+// ADC Static Functions
+static void adc_init(void) {
+    // ADC INIT
+    int err;
+
+    if (!adc_is_ready_dt(adc_channel)) {
+        printk("ADC controller device %s not ready\n", adc_channel->dev->name);
+        return;
+    }
+
+    err = adc_channel_setup_dt(adc_channel);
+    if (err < 0) {
+        printk("Could not setup ADC channel (%d)\n", err);
+        return;
+    }
+    
+    return;
+}
+
+static void stomp_sensor_read(void) {
+    int err;
+    int32_t val_mv;
+    printk("ADC reading[%u]:\n", count++);
+    printk("- %s, channel %d: ",
+            adc_channel->dev->name,
+            adc_channel->channel_id);
+
+    (void)adc_sequence_init_dt(adc_channel, &sequence);
+
+    err = adc_read_dt(adc_channel, &sequence);
+    if (err < 0) {
+        printk("Could not read (%d)\n", err);
+        return;
+    }
+
+    val_mv = (int32_t)buf;
+    err = adc_raw_to_millivolts_dt(adc_channel, &val_mv);
+    /* conversion to mV may not be supported, skip if not */
+    if (err < 0) {
+        printk(" (value in mV not available)\n");
+    } else {
+        printk("%d mV\n", val_mv);
+    }
+
+    if (val_mv > STOMP_SIGNAL_ON_THRESHOLD) {
+        stomp_signal = false; // Set stomp signal ON
+        printk("Stomp signal ON\n");
+    } else if (val_mv < STOMP_SIGNAL_OFF_THRESHOLD) {
+        stomp_signal = true; // Set stomp signal OFF
+        printk("Stomp signal OFF\n");
+    } else {
+        printk("Stomp signal unchanged\n");
+    }
+}
+
+static bool stomp_signal_received(void) {
+    // save old stomp signal state
+    static bool old_stomp_signal = false;
+
+    if (stomp_signal && !old_stomp_signal) {
+        old_stomp_signal = true;
+        return true;
+    } else if (!stomp_signal && old_stomp_signal) {
+        old_stomp_signal = false;
+        return false;
+    }
+    return false;
+}
+
 int main(void)
 {
     printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
@@ -97,79 +180,20 @@ int main(void)
     uart_thread_start();
     led_rgb_thread_start();
 
-    // ADC INIT
-    int err;
-	uint32_t count = 0;
-	uint16_t buf;
-	struct adc_sequence sequence = {
-		.buffer = &buf,
-		/* buffer size in bytes, not number of samples */
-		.buffer_size = sizeof(buf),
-	};
+    adc_init();
 
-	/* Configure channels individually prior to sampling. */
-	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-		if (!adc_is_ready_dt(&adc_channels[i])) {
-			printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
-			return 0;
-		}
-
-		err = adc_channel_setup_dt(&adc_channels[i]);
-		if (err < 0) {
-			printk("Could not setup channel #%d (%d)\n", i, err);
-			return 0;
-		}
-	}
 
     while (1) {
         handle_hmi_msg();
 
         // Send message to the LED RGB Thread
-        if (stomp_signal) {
+        if (stomp_signal_received()) {
             send_msg_main_led_rgb();
         }
 
         // ADC Reading
-		printk("ADC reading[%u]:\n", count++);
-		for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-			int32_t val_mv;
-
-			printk("- %s, channel %d: ",
-			       adc_channels[i].dev->name,
-			       adc_channels[i].channel_id);
-
-			(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
-
-			err = adc_read_dt(&adc_channels[i], &sequence);
-			if (err < 0) {
-				printk("Could not read (%d)\n", err);
-				continue;
-			}
-
-			/*
-			 * If using differential mode, the 16 bit value
-			 * in the ADC sample buffer should be a signed 2's
-			 * complement value.
-			 */
-			if (adc_channels[i].channel_cfg.differential) {
-				val_mv = (int32_t)((int16_t)buf);
-			} else {
-				val_mv = (int32_t)buf;
-			}
-			printk("%"PRId32, val_mv);
-			err = adc_raw_to_millivolts_dt(&adc_channels[i],
-						       &val_mv);
-			/* conversion to mV may not be supported, skip if not */
-			if (err < 0) {
-				printk(" (value in mV not available)\n");
-			} else {
-				printk(" = %"PRId32" mV\n", val_mv);
-			}
-		}
-
-		k_sleep(K_MSEC(1000));
-
-
+        stomp_sensor_read();
+    	k_sleep(K_MSEC(10));
 
         //printk("Main Thread Running\n");
         //k_msleep(50);
