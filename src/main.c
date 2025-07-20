79a0c3c82b187ef5ca0a9402d6a113108054f562
Gabriel Kis <gabrielkis65@gmail.com>
2025-07-20 17:10:22 -0300

    Added ADC to GPIO_1

--- src/main.c
+++ src/main.c
@@ -8,17 +8,41 @@
 
 #include <errno.h>
 #include <string.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 
-#define LOG_LEVEL 4
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main);
+#include <zephyr/drivers/adc.h>
+
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/util.h>
 
 #include "uart_cmd.h"
 #include "led_rgb_strip.h"
 #include "message_hmi_main.h"
 #include "message_main_ledrgb.h"
 
-// static uint16_t stomp_cnt = 0;
+// LOG Configuration
+#define LOG_LEVEL 4
+LOG_MODULE_REGISTER(main);
+
+
+// ADC Configuration
+#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
+	!DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
+#error "No suitable devicetree overlay specified"
+#endif
+
+#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
+	ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
+
+/* Data of ADC io-channels specified in devicetree. */
+static const struct adc_dt_spec adc_channels[] = {
+	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
+			     DT_SPEC_AND_COMMA)
+};
+
+// Global variables
 static bool stomp_signal = false;
 
 void handle_hmi_msg(void)
@@ -73,6 +97,30 @@ int main(void)
     uart_thread_start();
     led_rgb_thread_start();
 
+    // ADC INIT
+    int err;
+	uint32_t count = 0;
+	uint16_t buf;
+	struct adc_sequence sequence = {
+		.buffer = &buf,
+		/* buffer size in bytes, not number of samples */
+		.buffer_size = sizeof(buf),
+	};
+
+	/* Configure channels individually prior to sampling. */
+	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+		if (!adc_is_ready_dt(&adc_channels[i])) {
+			printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
+			return 0;
+		}
+
+		err = adc_channel_setup_dt(&adc_channels[i]);
+		if (err < 0) {
+			printk("Could not setup channel #%d (%d)\n", i, err);
+			return 0;
+		}
+	}
+
     while (1) {
         handle_hmi_msg();
 
@@ -81,8 +129,50 @@ int main(void)
             send_msg_main_led_rgb();
         }
 
+        // ADC Reading
+		printk("ADC reading[%u]:\n", count++);
+		for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+			int32_t val_mv;
+
+			printk("- %s, channel %d: ",
+			       adc_channels[i].dev->name,
+			       adc_channels[i].channel_id);
+
+			(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
+
+			err = adc_read_dt(&adc_channels[i], &sequence);
+			if (err < 0) {
+				printk("Could not read (%d)\n", err);
+				continue;
+			}
+
+			/*
+			 * If using differential mode, the 16 bit value
+			 * in the ADC sample buffer should be a signed 2's
+			 * complement value.
+			 */
+			if (adc_channels[i].channel_cfg.differential) {
+				val_mv = (int32_t)((int16_t)buf);
+			} else {
+				val_mv = (int32_t)buf;
+			}
+			printk("%"PRId32, val_mv);
+			err = adc_raw_to_millivolts_dt(&adc_channels[i],
+						       &val_mv);
+			/* conversion to mV may not be supported, skip if not */
+			if (err < 0) {
+				printk(" (value in mV not available)\n");
+			} else {
+				printk(" = %"PRId32" mV\n", val_mv);
+			}
+		}
+
+		k_sleep(K_MSEC(1000));
+
+
+
         //printk("Main Thread Running\n");
-        k_msleep(50);
+        //k_msleep(50);
     }
 
     return 0;
